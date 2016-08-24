5bfde465d5c938fb306a8da84df161506955b42d
Hong Gao <honggao@nyu.edu>
2016-08-24 16:10:05 -0400

    Add error handling

--- NeighborhoodMap/R_installpackages.R
+++ NeighborhoodMap/R_installpackages.R
@@ -1,16 +1,27 @@
 repos= "http://cran.us.r-project.org"
-install.packages('rgdal',repos=repos)
-install.packages('raster',repos=repos)
-install.packages('jsonlite',repos=repos)
-#dyn.load('/install.packages/Java/JavaVirtualMachines/jdk1.8.0_91.jdk/Contents/Home/jre/lib/server/libjvm.dylib')
-install.packages('rJava',repos=repos)
-install.packages('foreach',repos=repos)
-install.packages('doParallel',repos=repos)
-install.packages('rgeos',repos=repos)
-install.packages('geojsonio',repos=repos)
-install.packages('httr',repos=repos)
-install.packages('Hmisc',repos=repos)
-install.packages('ggplot2',repos=repos)
-install.packages('lubridate',repos=repos)
-install.packages('plyr',repos=repos)
-install.packages('sp',repos=repos)
+packages<- c('rgdal','raster','jsonlite','rJava','foreach',
+             'doParallel','rgeos','geojsonio','httr','Hmisc',
+             'ggplot2','lubridate','plyr','sp')
+
+for (i in packages){
+  tryCatch({
+    install.packages(i,repos=repos)
+  }, error=function(e){stop(paste0("Package",i,"fails to install."))})
+}
+
+# 
+# install.packages('rgdal',repos=repos)
+# install.packages('raster',repos=repos)
+# install.packages('jsonlite',repos=repos)
+# #dyn.load('/install.packages/Java/JavaVirtualMachines/jdk1.8.0_91.jdk/Contents/Home/jre/lib/server/libjvm.dylib')
+# install.packages('rJava',repos=repos)
+# install.packages('foreach',repos=repos)
+# install.packages('doParallel',repos=repos)
+# install.packages('rgeos',repos=repos)
+# install.packages('geojsonio',repos=repos)
+# install.packages('httr',repos=repos)
+# install.packages('Hmisc',repos=repos)
+# install.packages('ggplot2',repos=repos)
+# install.packages('lubridate',repos=repos)
+# install.packages('plyr',repos=repos)
+# install.packages('sp',repos=repos)
